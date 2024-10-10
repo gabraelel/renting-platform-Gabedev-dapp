4dca55ade4ba51b32679ff0fc94e4261648febc6
Gabrael EL <gabraelel@gmail.com>
2024-10-10 12:35:19 -0700

    Update App.js

--- renting-platform-Gabedev/src/App.js
+++ renting-platform-Gabedev/src/App.js
@@ -1,47 +1,36 @@
-import { BrowserRouter } from "react-router-dom";
-import AboutUs from "./pages/AboutUs";
-import NavBar from "./components/navbar/Navbar";
-import Developers from "./pages/Developers";
-import Footer from "./pages/Footer";
-import Join from "./pages/Join";
-import Loading from "./pages/Header";
-import Partners from "./pages/Partners";
-import Properties from "./pages/Properties";
-import Subscribe from "./pages/Subscribe";
 import React from 'react';
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter for routing
+
+import AboutUs from "./pages/AboutUs"; // AboutUs page component
+import NavBar from "./components/navbar/Navbar"; // Navbar component
+import Developers from "./pages/Developers"; // Developers page component
+import Footer from "./pages/Footer"; // Footer component
+import Join from "./pages/Join"; // Join page component
+import Loading from "./pages/Header"; // Header/Loading component
+import Partners from "./pages/Partners"; // Partners page component
+import Properties from "./pages/Properties"; // Properties page component for property listings
+import Subscribe from "./pages/Subscribe"; // Subscribe page component
+
 import BNBPriceDisplay from './BNBPriceDisplay'; // Import the BNB price display component
-import Properties from './Properties'; // Import the shuffled properties component
 
+// App component definition
 const App = () => {
-  return (
-    <div>
-      {/* Display BNB price at the top */}
-      <BNBPriceDisplay />
-      {/* Display shuffled property cards */}
-      <Properties />
-    </div>
-  );
-};
-
-export default App;
-
-
-function App() {
   return (
     <>
       <BrowserRouter>
-        <NavBar />
-        <Loading />
-        <Partners />
-        <Properties />
-        <AboutUs />
-        <Developers />
-        <Join />
-        <Subscribe />
-        <Footer />
+        <NavBar />          {/* Navigation bar */}
+        <Loading />         {/* Loading/Header component */}
+        <BNBPriceDisplay /> {/* Display BNB price at the top */}
+        <Partners />        {/* Partners section */}
+        <Properties />      {/* Property cards section */}
+        <AboutUs />         {/* About Us section */}
+        <Developers />      {/* Developers section */}
+        <Join />            {/* Join section */}
+        <Subscribe />       {/* Subscribe section */}
+        <Footer />          {/* Footer section */}
       </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
